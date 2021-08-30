68784a02d432458a589bd85e11ed9973d137b0e4
MishchenkoAnastasiia <anastasia.mishchenko@uipath.com>
2021-08-30 20:45:49 +0300

    Auto test added

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,34 @@
+import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void Test() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ithillel.ua/");
+        By buttonFreeConsultationPath = By.id("btn-consultation-hero");
+        WebElement buttonFreeConsultation = driver.findElement(buttonFreeConsultationPath);
+        buttonFreeConsultation.click();
+
+        By fieldNameConsultationPath = By.id("input-name-consultation");
+        WebElement fieldNameConsultation = driver.findElement(fieldNameConsultationPath);
+        fieldNameConsultation.click();
+        fieldNameConsultation.sendKeys("Test");
+
+        Assert.assertTrue(fieldNameConsultation.getText().equals("Test"));
+
+
+
+ //       driver.quit();
+        System.out.println("Successful");
+    }
+
+}
